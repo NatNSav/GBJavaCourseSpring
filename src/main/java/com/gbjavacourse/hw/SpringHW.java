3c515e0734f85b90b5ca07094b32aaae25c2cba1
NatNSav <nataly.kosareva@gmail.com>
2019-11-22 16:52:55 +0800

    5

--- src/main/java/com/gbjavacourse/hw/SpringHW.java
+++ src/main/java/com/gbjavacourse/hw/SpringHW.java
@@ -1,4 +1,9 @@
 package com.gbjavacourse.hw;
 
 public class SpringHW {
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println("test1");
+        System.out.println("test2");
+    }
 }
--- src/main/java/com/gbjavacourse/hw/Test.java
+++ src/main/java/com/gbjavacourse/hw/Test.java
@@ -1,4 +1,10 @@
 package com.gbjavacourse.hw;
 
 public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println("test1444444444444444444");
+
+    }
 }
